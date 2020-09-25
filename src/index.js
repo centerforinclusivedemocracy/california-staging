27272b90570c6d567e5a958ec4a6671b52550ff1
ScottStetkiewicz <themuppetman66@yahoo.com>
2020-09-24 20:20:41 -0400

    Revert "tri color setup for CA"
    This reverts commit c8680686959ede98e15691e2b56b7d84af5c5283.

--- src/index.js
+++ src/index.js
@@ -60,12 +60,10 @@ function initStatewideMap () {
 
                 switch (countyinfo.profile) {
                     case 'fullmodelGIN':
-                        return BOUNDSTYLE_FULLGIN;
                     case 'fullmodelCID':
-                        return BOUNDSTYLE_FULLCID;
                     case 'fullmodelTEMP':
-                    // case 'fullexceptsuggested':
-                    //     return BOUNDSTYLE_FULL;
+                    case 'fullexceptsuggested':
+                        return BOUNDSTYLE_FULL;
                     case 'lite':
                         return BOUNDSTYLE_LITE;
                     default:
